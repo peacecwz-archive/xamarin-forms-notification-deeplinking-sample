fbe5c3f2c392dbff112be533d5d345c16205e321
Barış Ceviz <me@barisceviz.com>
2018-09-22 02:49:42 +0300

    Created MainPage

---
+++ NotificationsSample/NotificationsSample/Views/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace NotificationsSample.Views
+{
+	public partial class MainPage : ContentPage
+	{
+	    private string Id = "test-xamarin";
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+	    protected override void OnAppearing()
+	    {
+	        base.OnAppearing();
+
+	        var url = $"https://barisceviz.com/{Id.ToString()}";
+
+	        var entry = new AppLinkEntry
+	        {
+	            Title = "Test Xamarin",
+	            Description = "Deep linking test",
+	            AppLinkUri = new Uri(url, UriKind.RelativeOrAbsolute),
+	            IsLinkActive = true,
+	        };
+
+	        Application.Current.AppLinks.RegisterLink(entry);
+
+        }
+    }
+}
