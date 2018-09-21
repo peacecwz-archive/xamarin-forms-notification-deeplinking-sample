fd8707720ba131afbb6f7fa24d346deb704cd61f
Barış Ceviz <me@barisceviz.com>
2018-09-22 02:49:28 +0300

    Created NotificationSummaryPage

---
+++ NotificationsSample/NotificationsSample/Views/NotificationSummaryPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NotificationsSample.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NotificationSummaryPage : ContentPage
+	{
+		public NotificationSummaryPage (string labelText)
+		{
+			InitializeComponent ();
+		    lblText.Text = labelText;
+		}
+	}
+}
